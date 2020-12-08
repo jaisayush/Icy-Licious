0d7a129dc99607ce5f002127d2995f8f6d87d18f
Ayush Jaiswal <ayush.jaiswal2@mindtree.com>
2020-12-08 12:52:00 +0530

    added all the display for icecream

--- src/admin/view-product/view-product/view-product.component.ts
+++ src/admin/view-product/view-product/view-product.component.ts
@@ -1,15 +1,18 @@
+import { ViewProductService } from './../view-product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-product',
   templateUrl: './view-product.component.html',
-  styleUrls: ['./view-product.component.scss']
+  styleUrls: ['./view-product.component.scss'],
 })
 export class ViewProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public products;
+  constructor(private service: ViewProductService) {
+    this.service.getProducts().subscribe((response) => {
+      this.products = response;
+    });
   }
 
+  ngOnInit(): void {}
 }
