33f4728bc0da6e59aae2a43cebc5de1566bc85e6
MOHAMED SHEHAS V <mohamedshehas.v2@mindtree.com>
2020-12-09 10:41:09 +0530

    added update pop up

--- src/admin/view-product/view-product.module.ts
+++ src/admin/view-product/view-product.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ViewProductComponent } from './view-product/view-product.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ViewProductComponent],
-  imports: [CommonModule,MatCardModule,MatButtonModule],
+  imports: [CommonModule,MatCardModule,MatButtonModule,ReactiveFormsModule],
   exports: [ViewProductComponent],
 })
 export class ViewProductModule {}
